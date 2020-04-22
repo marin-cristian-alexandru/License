5ce3a51d1d3a9c47e6c6ebb9129d5404215c3a31
Marin Alexandru <cristian.marin0805@stud.acs.upb.ro>
2020-04-22 14:03:13 +0300

    added pineapple in the dataset

--- rename.sh
+++ rename.sh
@@ -2,6 +2,7 @@
 
 # Script for formatting images with the name and coresponding label
 FRUIT=$1
+#initial value for counter (first image renamed will have i+1 index value)
 i=0
 for f in $(ls -v | grep .jpg)
 do
